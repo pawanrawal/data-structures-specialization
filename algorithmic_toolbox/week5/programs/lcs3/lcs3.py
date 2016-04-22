32d4bc0e2e879edd83da8166c76bb766fff40e17
Pawan Rawal <pawan0201@gmail.com>
2016-04-22 14:56:22 +0530

    correct solution for last problem

--- algorithmic_toolbox/week5/programs/lcs3/lcs3.py
+++ algorithmic_toolbox/week5/programs/lcs3/lcs3.py
@@ -2,41 +2,20 @@
 
 import sys
 
-def commonseq(i,j,A,a):
-    if i == 0 and j == 0:
-        return
-    if i > 0 and A[i][j] == A[i-1][j]:
-        commonseq(i-1,j,A,a)
-    elif j>0 and A[i][j] == A[i][j-1]:
-        commonseq(i,j-1,A,a)
-    else:
-        if A[i][j] == A[i-1][j-1] + 1:
-            # If we have a match of a[i-1] and b[j-1] ,then we append that entry
-            # to our substr list.
-            substr.append(a[i-1])
-        commonseq(i-1, j-1,A,a)
-
-
-def lcs2(a, b):
-    global substr
-    substr = []
+def lcs3(a, b,c):
     # A represents alignment score
-    A = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
+    A = [[[0 for j in range(len(c)+1)] for i in range(len(b)+1)] for k in range(len(a)+1)]
     for i in range(1, len(a)+1):
         for j in range(1, len(b)+1):
-            if a[i-1] == b[j-1]:
-                A[i][j] = max(A[i-1][j-1]+1, A[i-1][j], A[i][j-1])
-            else:
-                A[i][j] = max(A[i-1][j-1], A[i-1][j], A[i][j-1])
+            for k in range(1, len(c)+1):
+                if a[i-1] == b[j-1] == c[k-1]:
+                    A[i][j][k] = max(A[i-1][j-1][k-1]+1, A[i][j-1][k],A[i-1][j][k], A[i-1][j-1][k],
+                        A[i-1][j][k-1], A[i][j-1][k-1], A[i][j][k-1])
+                else:
+                    A[i][j][k] = max(A[i-1][j-1][k-1], A[i][j-1][k],A[i-1][j][k], A[i-1][j-1][k],
+                        A[i-1][j][k-1], A[i][j-1][k-1], A[i][j][k-1])
 
-    commonseq(len(a),len(b),A,a)
-    return list(reversed(substr))
-
-def lcs3(a, b, c):
-    commonAB = lcs2(a,b)
-    commonBC = lcs2(b,c)
-    commonABC = lcs2(commonAB, commonBC)
-    return len(commonABC)
+    return A[len(a)][len(b)][len(c)]
 
 if __name__ == '__main__':
     global a,b,c
